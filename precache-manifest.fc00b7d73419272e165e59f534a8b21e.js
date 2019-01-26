52978517a37a2f5720e3e179a5e400aac993511a
Wynand Badenhorst <wynandbadenhorst@hotmail.com>
2019-01-26 11:14:31 -0600

    Updates

---
+++ precache-manifest.fc00b7d73419272e165e59f534a8b21e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages-template/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "58b0217f82efabb30b93",
+    "url": "/react-gh-pages-template/static/js/runtime~main.58b0217f.js"
+  },
+  {
+    "revision": "a03a660406b312ba9ffc",
+    "url": "/react-gh-pages-template/static/js/main.a03a6604.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react-gh-pages-template/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "a03a660406b312ba9ffc",
+    "url": "/react-gh-pages-template/static/css/main.5fc0b1b3.chunk.css"
+  },
+  {
+    "revision": "44eb3adfdba94a7799528954059a2c07",
+    "url": "/react-gh-pages-template/index.html"
+  }
+];
